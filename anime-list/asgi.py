27174620946b9f84b1d86cd3ca7ca88b98f561c2
Someone-tensai <paudelsulav5@gmail.com>
2026-02-05 20:34:30 +0545

    Fixing

--- anime-list/asgi.py
+++ anime-list/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJECT_NAME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anime-list.settings')
 
 application = get_asgi_application()
